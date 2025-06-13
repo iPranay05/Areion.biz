8c401f6192bfb1db15491f7ab404fd92d2eac577
Pranay <pranaynair05@gmail.com>
2025-06-13 15:04:00 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
 import ServicesSection from '@/components/ServicesSection';
-import PacksSection from '@/components/PacksSection';
+// import PacksSection from '@/components/PacksSection';
 import TeamSection from '@/components/TeamSection';
 import TestimonialsSection from '@/components/TestimonialsSection';
 import ContactSection from '@/components/ContactSection';
